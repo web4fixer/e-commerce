0e6db765b3ed00a7a19ce49496ce665a2ec20289
web4fixer <web4fixer@gmail.com>
2018-10-16 19:58:46 +0300

    header top markup

---
+++ app/js/common.js
@@ -0,0 +1,16 @@
+
+$('.notify-bell').click(function() {
+	$('.dropdown-notify-menu').toggle();
+})
+
+$('.notify-message').click(function() {
+	$('.dropdown-message-menu').toggle();
+});
+
+$('.notify-cart').click(function() {
+	$('.dropdown-cart-menu').toggle();
+});
+
+$('.account-user-block').click(function() {
+	$('.dropdown-user-menu').toggle();
+});
